03bcbfa96d68c362f2d5da1e1a4abc8ae4179ed9
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-05-11 14:42:59 -0400

    Completed all code. Tested all CLI options, ORM functions, and Properties

--- lib/cli.py
+++ lib/cli.py
@@ -62,143 +62,198 @@ def menu(ctx):
     elif choice == '12':
         ctx.invoke(display_all_guests)
     elif choice == '13':
-        ctx.invoke(search_for_guests_within_one_hotel)
+        ctx.invoke(search_for_guests_from_one_hotel)
     elif choice == '14':
         exit_program()
 
-# @clear_screen("New Hotel Created")
+
 @cli.command()
 @click.option('--name', default=None, help='Name of hotel.')
 @click.option('--location', default=None, help='Location of hotel.')
 def create_hotel(name=None, location=None):
     """Create a new hotel."""
-    styled_dashes_text("Creating a hotel")
-    ascii_symbols("hotel")
-    hotel_name = click.prompt("\nEnter the name of the hotel", type=str) if name == None else name
-    hotel_location = click.prompt("Location of the hotel", type=str) if location == None else location
-    return Hotel.create(hotel_name, hotel_location)
-    
+    @clear_screen("New Hotel Created")
+    def wrapper():
+        styled_dashes_text("Creating a hotel")
+        ascii_symbols("hotel")
+        hotel_name = click.prompt("\nEnter the name of the hotel", type=str) if name == None else name
+        hotel_location = click.prompt("Location of the hotel", type=str) if location == None else location
+        return Hotel.create(hotel_name, hotel_location)
+    return wrapper() 
     
 @cli.command()
-@clear_screen("Hotel Updated")
-def update_hotel():
+@click.option('--id', default=None, help='Updated name of hotel.')
+@click.option('--location', default=None, help='Updated location of hotel.')
+def update_hotel(id: int = None, name:str = None, location: str = None):
     """Update an existing hotel."""
-    styled_dashes_text("Updating a hotel")
+    @clear_screen("Hotel Updated")
+    def wrapper():
+        styled_dashes_text("Updating a hotel")
+        print(all_hotels_in_db()) if id == None else None
+        hotel_selected = Hotel.find_by_id(click.prompt("\nEnter the hotel id from the list of available hotels above", type=int)) if id == None else Hotel.find_by_id(int(id))
+        click.echo(f"Hotel Selected: {hotel_selected}") if name == None else None
+        new_hotel_name = input(f"\nEnter the new name for the Hotel selected: {hotel_selected}: ") if name == None else name
+        new_hotel_location = input(f"\nEnter the new location for the Hotel selected: {hotel_selected}: ") if location == None else location
+        return hotel_selected.update(new_hotel_name, new_hotel_location)
     
-    for hotel in Hotel.get_all():
-        print(hotel)
-    hotel_selected = Hotel.find_by_id(click.prompt("\nEnter the hotel id from the list of available hotels above", type=int))
-    click.echo(f"Hotel Selected: {hotel_selected}")
-    new_hotel_name = input(f"\nEnter the new name for the Hotel selected: {hotel_selected}: ")
-    new_hotel_location = input(f"\nEnter the new location for the Hotel selected: {hotel_selected}: ")
-    return hotel_selected.update(new_hotel_name, new_hotel_location)
-    
+    return wrapper() 
+
 @cli.command()
-@clear_screen("Hotel Deleted")
-def delete_hotel():
+@click.option('--id', default=None, help='ID of hotel to delete.')
+def delete_hotel(id:int = None):
     """Delete a hotel by ID."""
-    styled_dashes_text("Deleting a hotel")
-    for hotel in Hotel.get_all():
-            print(hotel)
-    hotel_selected = Hotel.find_by_id(click.prompt("\nEnter the hotel id from the list of available hotels above", type=int))
-    return hotel_selected.delete()
+    
+    @clear_screen("Hotel Deleted")
+    def wrapper():
+        styled_dashes_text("Deleting a hotel")
+        print(all_hotels_in_db()) if id == None else None
+        hotel_selected = Hotel.find_by_id(click.prompt("\nEnter the hotel id from the list of available hotels above", type=int)) if id == None else Hotel.find_by_id(int(id))
+        return hotel_selected.delete()
+    return wrapper()
 
 
 @cli.command()
-@clear_screen("Displaying All Hotels")
 def display_all_hotels():
     """Display all hotels."""
-    styled_dashes_text("Displaying all hotels")
-    return all_hotels_in_db()      
+    @clear_screen("Displaying All Hotels")
+    def wrapper():
+        styled_dashes_text("Displaying all hotels")
+        return all_hotels_in_db()  
+    return wrapper()    
         
 @cli.command()
-@clear_screen("Found Entries For Hotel Searched By Name", True)
-def search_hotel_by_name():
+@click.option('--name', default=None, help='Name of hotel name to search.')
+def search_hotel_by_name(name=None):
     """Search for a hotel by name via fuzzy search"""
-    hotel_name_to_search = click.prompt("\nEnter the name of the hotel to search")
-    hotel_matches = ""
-    for index, entry in enumerate(fuzzy_match(hotel_name_to_search, Hotel.get_all())):
-        hotel_matches += f"{index+1}. {entry}\n"
-    return hotel_matches
+    
+    @clear_screen("Found Entries For Hotel Searched By Name", True)
+    def wrapper():
+        hotel_name_to_search = click.prompt("\nEnter the name of the hotel to search") if name == None else name
+        hotel_matches = ""
+        for index, entry in enumerate(fuzzy_match(hotel_name_to_search, Hotel.get_all())):
+            hotel_matches += f"{index+1}. {entry}\n"
+        return hotel_matches
+    return wrapper()
     
 @cli.command()
-@clear_screen("Found Entry For Hotel Searched By ID")
-def search_hotel_by_id():
+@click.option('--id', default=None, help='ID of hotel name to search.')
+def search_hotel_by_id(id):
     """Search for a hotel by ID"""
-    return Hotel.find_by_id(click.prompt("\nEnter the ID of the hotel to search", type=int))
+    
+    @clear_screen("Found Entry For Hotel Searched By ID")
+    def wrapper():
+        return Hotel.find_by_id(click.prompt("\nEnter the ID of the hotel to search", type=int)) if id == None else Hotel.find_by_id(int(id))
+
+    return wrapper()
 
 @cli.command()
-@clear_screen("Created A New Guest")
-def create_guest():
+@click.option('--id', default=None, help='ID of hotel new guest will be staying at.')
+@click.option('--name', default=None, help='Name of new guest.')
+def create_guest(id=None, name=None):
     """Create a new guest."""
-    styled_dashes_text("Creating a guest")
-    print(all_hotels_in_db(), end="\n\n")
-    guest_hotel_id = click.prompt("Enter the ID of the hotel, the guest is staying at", type=int)
-    guest_name = click.prompt("\nEnter the name of the guest", type=str)
-    return Guest.create(guest_name, guest_hotel_id)
+    
+    @clear_screen("Created A New Guest")
+    def wrapper():
+        styled_dashes_text("Creating a guest")
+        print(all_hotels_in_db(), end="\n\n") if id == None else None
+        guest_hotel_id = click.prompt("Enter the ID of the hotel, the guest is staying at", type=int) if id == None else int(id)
+        guest_name = click.prompt("\nEnter the name of the guest", type=str) if name == None else name
+        return Guest.create(guest_name, guest_hotel_id)
+    return wrapper()
 
 
 @cli.command()
-@clear_screen("Updated A Guest")
-def update_guest():
+@click.option('--gid', default=None, help='ID of Guest to update.')
+@click.option('--hid', default=None, help='ID of updated Hotel guest will stay at.')
+@click.option('--name', default=None, help='Updated name of new guest.')
+def update_guest(gid:int=None, hid:int=None, name:str=None):
     """Update an existing guest."""
-    styled_dashes_text("Updating A Guest")
-    print(all_guests_in_db(), end="\n\n")
-    guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you want to update", type=int))
-    guest_name = click.prompt("\nEnter the updated name of the guest", type=str)
-    print(all_hotels_in_db(), end="\n\n")
-    guest_hotel_id = click.prompt("\nEnter the updated ID of the hotel our guest is staying at", type=int)
-    return guest.update(guest_name, guest_hotel_id)
+    
+    @clear_screen("Updated A Guest")
+    def wrapper():
+        styled_dashes_text("Updating A Guest")
+        print(all_guests_in_db(), end="\n\n") if gid == None else None
+        guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you want to update", type=int)) if hid == None else Guest.find_by_id(int(hid))
+        guest_name = click.prompt("\nEnter the updated name of the guest", type=str) if name == None else name
+        print(all_hotels_in_db(), end="\n\n") if hid == None else None
+        guest_hotel_id = click.prompt("\nEnter the updated ID of the hotel our guest is staying at", type=int) if hid == None else int(hid)
+        return guest.update(guest_name, guest_hotel_id)
+    
+    return wrapper()
 
 
 @cli.command()
-@clear_screen("Deleted A Guest")
-def delete_guest():
+@click.option('--id', default=None, help='ID of Guest to delete.')
+def delete_guest(id=None):
     """Delete a guest."""
-    styled_dashes_text("Deleting A Guest")
-    print(all_guests_in_db(), end="\n\n")
-    guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you want to delete", type=int))
-    guest.delete()
-    return guest
+    
+    @clear_screen("Deleted A Guest")
+    def wrapper():
+        styled_dashes_text("Deleting A Guest")
+        print(all_guests_in_db(), end="\n\n") if id == None else None
+        guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you want to delete", type=int)) if id == None else Guest.find_by_id(int(id))
+        guest.delete()
+        return guest
+
+    return wrapper()
 
 @cli.command()
-@clear_screen("Displaying All Guests")
 def display_all_guests():
     """Display all hotels."""
-    styled_dashes_text("Displaying all hotels")
-    return all_guests_in_db()
+    
+    @clear_screen("Displaying All Guests")
+    def wrapper():
+        styled_dashes_text("Displaying all hotels")
+        return all_guests_in_db()
+    
+    return wrapper()
 
 @cli.command()
-@clear_screen("Found Entry For Guest Searched By ID")
-def search_for_guest_by_id():
+@click.option('--id', default=None, help='ID of Guest to find.')
+def search_for_guest_by_id(id=None):
     """Search for a guest."""
-    styled_dashes_text("Searching A Guest By ID")
-    print(all_guests_in_db(), end="\n\n")
-    guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you are searching for", type=int))
-    return guest
+    
+    @clear_screen("Found Entry For Guest Searched By ID")
+    def wrapper():
+        styled_dashes_text("Searching A Guest By ID")
+        print(all_guests_in_db(), end="\n\n")
+        guest = Guest.find_by_id(click.prompt("Enter the ID of the guest you are searching for", type=int)) if id == None else Guest.find_by_id(int(id)) 
+        return guest
+    
+    return wrapper() 
 
 @cli.command()
-@clear_screen("Found Entries For Guest Searched By Name")
-def search_for_guest_by_name():
+@click.option('--name', default=None, help='Name of Guest to find.')
+def search_for_guest_by_name(name=None):
     """Search for a guest by name via fuzzy search."""
-    styled_dashes_text("Searching A Guest By Name")
-    guest_name_to_search = input("\nEnter the name of the guest: ")
-    guest_matches = ""
-    for index, entry in enumerate(fuzzy_match(guest_name_to_search, Guest.get_all())):
-        guest_matches += f"{index+1}. {entry}\n"
-    return guest_matches
+    
+    @clear_screen("Found Entries For Guest Searched By Name")
+    def wrapper():
+        styled_dashes_text("Searching A Guest By Name")
+        guest_name_to_search = input("\nEnter the name of the guest: ") if name == None else name
+        guest_matches = ""
+        for index, entry in enumerate(fuzzy_match(guest_name_to_search, Guest.get_all())):
+            guest_matches += f"{index+1}. {entry}\n"
+        return guest_matches
+    
+    return wrapper()
 
 @cli.command()
-@clear_screen("Found Entries For Guests Within One Hotel")
-def search_for_guests_within_one_hotel():
+@click.option('--id', default=None, help='ID of hotel to return all guests from')
+def search_for_guests_from_one_hotel(name:str=None):
     """Displays all guests for a single hotel """
-    styled_dashes_text("Displaying All Guests Within One Hotel")
-    print(all_hotels_in_db())
-    hotel = Hotel.find_by_id(click.prompt("\nSelect the hotel to check their guest list", type=int))
-    guest_matches = ""
-    for index, entry in enumerate(hotel.guests()):
-        guest_matches += f"{index+1}. {entry}\n"
-    return guest_matches
+    
+    @clear_screen("Found Entries For Guests Within One Hotel")
+    def wrapper():
+        styled_dashes_text("Displaying All Guests Within One Hotel")
+        print(all_hotels_in_db())
+        hotel = Hotel.find_by_id(click.prompt("\nSelect the hotel to check their guest list", type=int))
+        guest_matches = ""
+        for index, entry in enumerate(hotel.guests()):
+            guest_matches += f"{index+1}. {entry}\n"
+        return guest_matches
+    
+    return wrapper() 
 
 if __name__ == "__main__":
     cli()
